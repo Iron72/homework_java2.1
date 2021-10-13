21bf6432ad7186f12938fe2cb8f11a6fdd82dff9
iron72 <iron72rus@gmail.com>
2021-10-13 10:09:16 +0300

    Calculation miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int costMile = 20;
+        int costTicket = 13_300;
+        int mile = costTicket / costMile;
+        System.out.println(mile + " Бонусых миль будет получено за покупку авиабилета");
+    }
+}
